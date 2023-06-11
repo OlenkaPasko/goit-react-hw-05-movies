afc09b09d7195f60ebb6a06de5c91f26fb64850c
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-06-11 14:02:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,17 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 export const App = () => {
   return (
     <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/movi">Movies</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/movi" element={<div>Movies</div>} />
+      </Routes>
     </div>
   );
 };
