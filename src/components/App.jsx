99f6a0bbcf79ddeadb9c39de80b9db7754a617ed
OlenkaPasko <lena_nechipurenko@ukr.net>
2023-06-11 17:49:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,13 +8,15 @@ export const App = () => {
           <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink to="/movi">Movies</NavLink>
+          <NavLink to="/movies">Movies</NavLink>
         </li>
       </ul>
-
       <Routes>
         <Route path="/" element={<div>Home</div>} />
-        <Route path="/movi" element={<div>Movies</div>} />
+        <Route path="movies" element={<div>Movies</div>} />
+        <Route path="movies/:movieId" element={<div>MovieDetailsPage</div>} />
+        <Route path="cast" element={<div>Cast</div>} />
+        <Route path="reviews" element={<div>Reviews</div>} />
       </Routes>
     </div>
   );
